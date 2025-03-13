66b3e62524e1201ceb23bebd029e69ca20a06b9b
kiano-serha <144807543+kiano-serha@users.noreply.github.com>
2025-03-13 06:24:04 -0500

    Delete frontEnd directory

--- frontEnd/src/Pages/Validators/RatingSchema.jsx
+++
@@ -1,9 +0,0 @@
-import * as yup from "yup";
-
-const RatingSchema = yup.object().shape({
-  movie_title: yup.string().required("Movie Title is a required field"),
-  description: yup.string().required("Description is a required field"),
-  num_of_stars: yup.string().required("Number of stars is required"),
-});
-
-export default RatingSchema;
--- frontEnd/src/Pages/components/Toast.jsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { Toaster } from "sonner";
-
-export default function Toast() {
-  return <Toaster richColors position="top-right" expand={true} />;
-}
