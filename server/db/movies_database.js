0b3d70ac197a2960818100fa0fb8b603f472b3c4
kiano-serha <144807543+kiano-serha@users.noreply.github.com>
2025-03-13 07:23:43 -0500

    Update movies_database.js

--- server/db/movies_database.js
+++ server/db/movies_database.js
@@ -1,12 +1,13 @@
 const mysql2 = require("mysql2/promise");
+require("dotenv").config();
 
 const db_connection = mysql2.createPool({
-  host: "localhost",
-  user: "root",
-  password: "",
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   database: "movies",
-  // port: "3306",
-  // connectionLimit: 100,
+  port: "21948",
+  connectionLimit: 100,
 });
 
 module.exports = db_connection;
